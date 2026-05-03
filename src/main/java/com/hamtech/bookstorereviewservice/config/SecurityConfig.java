8ec5a73d7fb6ab695399b4200987f7deae7beb96
HinDayNi <gthithanhhien04@gmail.com>
2026-05-03 11:38:09 +0700

    feat: implement core review functionality with repository, service, and security configurations

--- src/main/java/com/hamtech/bookstorereviewservice/config/SecurityConfig.java
+++ src/main/java/com/hamtech/bookstorereviewservice/config/SecurityConfig.java
@@ -59,6 +59,7 @@ public class SecurityConfig {
 
                         // Review
                         "/api/review/**",
+                        "/api/v1/reviews/**",
         };
 
         @Value("${jwt.signerKey}")
