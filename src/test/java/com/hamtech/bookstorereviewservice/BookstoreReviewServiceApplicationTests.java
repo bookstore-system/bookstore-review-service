e42ab86462f9a1f661e9379c9e42999e4bef57fb
HinDayNi <gthithanhhien04@gmail.com>
2026-05-08 14:56:09 +0700

    feat: enhance test configuration for BookstoreReviewServiceApplication with mock repository and custom properties

--- src/test/java/com/hamtech/bookstorereviewservice/BookstoreReviewServiceApplicationTests.java
+++ src/test/java/com/hamtech/bookstorereviewservice/BookstoreReviewServiceApplicationTests.java
@@ -2,9 +2,20 @@ package com.hamtech.bookstorereviewservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-@SpringBootTest
+import com.hamtech.bookstorereviewservice.repository.ReviewRepository;
+
+@SpringBootTest(classes = BookstoreReviewServiceApplication.class, properties = {
+        "spring.autoconfigure.exclude=" +
+                "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
+                "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration",
+        "jwt.signerKey=unit-test-signer-key"
+})
 class BookstoreReviewServiceApplicationTests {
+
+    @MockBean
+    ReviewRepository reviewRepository;
 
     @Test
     void contextLoads() {
